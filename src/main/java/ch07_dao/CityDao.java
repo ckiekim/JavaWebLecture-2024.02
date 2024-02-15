e85abfb85fe5872454dccdc8dc397dee60e214b5
Cheolkie Kim <ckiekim@naver.com>
2024-02-15 15:47:04 +0900

    DB access

--- src/main/java/ch07_dao/CityDao.java
+++ src/main/java/ch07_dao/CityDao.java
@@ -89,5 +89,37 @@ public class CityDao {
 			e.printStackTrace();
 		}
 	}
+
+	public void deleteCity(int id) {
+		Connection conn = getConnection();
+		String sql = "delete from kcity where id=?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setInt(1, id);
+			
+			pstmt.executeUpdate();
+			pstmt.close(); conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void updateCity(City city) {
+		Connection conn = getConnection();
+		String sql = "update kcity set name=?, countryCode=?, district=?, population=? where id=?";
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, city.getName());
+			pstmt.setString(2, city.getCountryCode());
+			pstmt.setString(3, city.getDistrict());
+			pstmt.setInt(4, city.getPopulation());
+			pstmt.setInt(5, city.getId());
+			
+			pstmt.executeUpdate();
+			pstmt.close(); conn.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
