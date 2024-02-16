5fc9588c085d920441d0ba7007742294c3d43a6b
Cheolkie Kim <ckiekim@naver.com>
2024-02-16 14:23:44 +0900

    DB Access

---
+++ src/main/java/ch07_dao/kpop/KpopDaoImpl.java
@@ -0,0 +1,61 @@
+package ch07_dao.kpop;
+
+import java.util.List;
+
+public class KpopDaoImpl implements KpopDao {
+
+	@Override
+	public List<Kpop> getKpopList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Artist getArtist(int aid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Song getSong(int sid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insertArtist(Artist artist) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void insertSong(Song song) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateArtist(Artist artist) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateSong(Song song) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteArtist(int aid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteSong(int sid) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
