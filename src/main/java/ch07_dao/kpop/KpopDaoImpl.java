a2f9c5b82dce8ed4b761d7a80a97a5fb0250b0c0
Cheolkie Kim <ckiekim@naver.com>
2024-02-19 09:51:20 +0900

    DB Access

--- src/main/java/ch07_dao/kpop/KpopDaoImpl.java
+++ src/main/java/ch07_dao/kpop/KpopDaoImpl.java
@@ -31,7 +31,15 @@ public class KpopDaoImpl implements KpopDao {
 		Connection conn = getConnection();
 		String sql = "SELECT g.*, s.title, s.lyrics FROM girl_group g"
 					+ "  JOIN song s ON g.hit_song_id=s.sid"
-					+ "  ORDER BY gid desc";
+					+ "  ORDER BY debut desc";
+//		String sql = "SELECT r.gid, r.name, r.debut, l.sid, l.title, l.lyrics FROM song l "
+//				+ "	LEFT OUTER JOIN girl_group r "
+//				+ "	ON l.sid=r.hit_song_id "
+//				+ "UNION "
+//				+ "SELECT r.gid, r.name, r.debut, l.sid, l.title, l.lyrics FROM song l "
+//				+ "	RIGHT OUTER JOIN girl_group r"
+//				+ "	ON l.sid=r.hit_song_id "
+//				+ "	ORDER BY debut desc";
 		List<Kpop> list = new ArrayList<Kpop>();
 		try {
 			Statement stmt = conn.createStatement();
