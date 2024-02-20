9c40d5049e859231bfd60853239831d6ca2296bc
Cheolkie Kim <ckiekim@naver.com>
2024-02-20 11:23:14 +0900

    Filter

--- src/main/java/ch10_filter/EncodingFilter.java
+++ src/main/java/ch10_filter/EncodingFilter.java
@@ -16,22 +16,22 @@ public class EncodingFilter extends HttpFilter implements Filter {
        
     public EncodingFilter() {
         super();
-        System.out.println("생성자");
+//		System.out.println("생성자");
     }
 
 	public void destroy() {
-		System.out.println("destroy()");
+//		System.out.println("destroy()");
 	}
 
 	// 필요한 작업을 코딩하는 부분
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		request.setCharacterEncoding("utf-8");
-		System.out.println("doFilter()");
+//		System.out.println("doFilter()");
 		chain.doFilter(request, response);
 	}
 
 	public void init(FilterConfig fConfig) throws ServletException {
-		System.out.println("init()");
+//		System.out.println("init()");
 	}
 
 }
