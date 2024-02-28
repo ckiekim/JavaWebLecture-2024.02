ed92a934d33284089aa8850829b836156f3dc213
Cheolkie Kim <ckiekim@naver.com>
2024-02-28 09:49:51 +0900

    Product

---
+++ src/main/java/project/controller/ProdcutController.java
@@ -0,0 +1,48 @@
+package project.controller;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+
+@WebServlet({"/bbs/product/insert", "/bbs/product/view"})
+@MultipartConfig(
+		fileSizeThreshold = 1 * 1024 * 1024,			// 1 MB
+		maxFileSize = 10 * 1024 * 1024,					// 10 MB
+		maxRequestSize = 10 * 1024 * 1024
+)
+public class ProdcutController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	public static final String UPLOAD_PATH = "c:/Temp/upload/bbs";
+       
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String[] uri = request.getRequestURI().split("/");
+		String action = uri[uri.length - 1];
+		String method = request.getMethod();
+		HttpSession session = request.getSession();
+		RequestDispatcher rd = null;
+		
+		switch(action) {
+		case "insert":
+			if (method.equals("GET")) {
+				rd = request.getRequestDispatcher("/WEB-INF/view/product/insert.jsp");
+				rd.forward(request, response);
+			} else {
+				String category = request.getParameter("category");
+				String pname = request.getParameter("pname");
+				String price_ = request.getParameter("category");
+				int price = Integer.parseInt(price_);
+				String description = request.getParameter("description");
+			}
+			break;
+			
+		}
+	}
+
+}
